6e5db03c82675115c0354e4da6854519935b81b8
lnoguera <lnoguera@frenkel.com.mx>
2024-01-19 17:04:37 -0600

    Adding and correcting authentication for MQTT server

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
 # Mosquitto autentication
-mosquitto_passwd -b ./mqtt/config/pwfile.txt $MQTT_USERNAME $MQTT_PASSWORD
+docker-compose up -d
+source .env
+echo -n "" > ./mqtt/config/pwfile
+chmod 0700 ./mqtt/config/pwfile
+docker exec mqtt mosquitto_passwd -H sha512-pbkdf2 -c -b ./mosquitto/config/pwfile "$MQTT_USERNAME" "$MQTT_PASSWORD"
